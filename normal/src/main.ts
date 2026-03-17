d8f048b976162235641c9dde5b79ecb58e50c85c
CHIMON-Thanatchon <ilovechimon@gmail.com>
2026-03-17 17:10:17 +0700

    Update main.ts

--- normal/src/main.ts
+++ normal/src/main.ts
@@ -2,6 +2,13 @@ import express from "express";
 import type { Request, Response } from "express";     // เราต้อง Import  'type' { Req , Res } from ... ; 
 const app = express();
 
+app.use(express.json());
+
+app.post('/user', (req: Request, res: Response) => {   // นำ Type มาระบุ Parameter
+    console.log(req.body)   // ถ้าไม่มี express.json() บรรทัดนี้จะกลายเป็น undefined  
+    res.send(`I've received the information.`)
+})
+
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World");
     req
